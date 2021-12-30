219c41d370a07f2411dd2d54fe137678f83d2094
Impervguin <leon53000@yandex.ru>
2021-12-30 18:00:58 +0300

    created fight and fightmember classes

--- Classes.py
+++ Classes.py
@@ -22,6 +22,10 @@ class BaseCharacter:
         self.XP = characteristics["XP"]
         self.Attack_type = characteristics["Attack_type"]
 
+        self.Spec_Attack_Modifiers = characteristics["Phys_Spec_Attack_Modifier"], characteristics[
+            "Mag_Spec_Attack_Modifier"]
+        self.Spec_Attack_Recovery = characteristics["Spec_Attack_Recovery"]
+
         self.Base_Stats = dict()
         self.Base_Stats["HP"] = 3600
         self.Base_Stats["PHYS_Atk"] = 100
@@ -56,8 +60,6 @@ class BaseCharacter:
             self.Stats[stat] *= int(1 + self.Level / 25)
             self.Stats[stat] += self.armor.Stat_Boosts[stat]
             self.Stats[stat] += self.weapon.Stat_Boosts[stat]
-
-        self.Stats["HP"] = self.Stats["Max_HP"]
 
     def Equip_armor(self, armor):
         if armor is None:
@@ -103,9 +105,27 @@ class BaseCharacter:
         target.Stats["HP"] -= total_damage
         return total_damage
 
+    def SpecAttack(self, target):
+        phys_damage_modifier, mag_damage_modifier = self.Spec_Attack_Modifiers
 
+        phys_damage = phys_damage_modifier * self.Stats["Phys_Atk"]
+        mag_damage = mag_damage_modifier * self.Stats["Mag_Atk"]
 
+        r = random.randint(0, 100)
+        if r <= self.Stats["Crit_Chance"]:
+            phys_damage *= self.Stats["Crit_Modifier"] / 100
+            mag_damage *= self.Stats["Crit_Modifier"] / 100
 
+        phys_damage -= target.Stats["Phys_Def"]
+        mag_damage -= target.Stats["Mag_Def"]
+
+        mag_damage = max(0, mag_damage)
+        phys_damage = max(0, phys_damage)
+
+        total_damage = int(mag_damage + phys_damage)
+
+        target.Stats["HP"] -= total_damage
+        return total_damage
 
 
 class EquipItem:
@@ -131,12 +151,59 @@ class Weapon(EquipItem):
         super(Weapon, self).__init__()
         self.Item_Class = "Weapon"
 
-# class FightMember:
-#     def __init__(self, character):
-#         self.character = character
-#         self.defeated = False
-#
-# class Fight:
-#     def __init__(self, command1, command2):
+
+class FightMember:
+    def __init__(self, character: BaseCharacter):
+        self.character = character
+        self.defeated = False
+        self.made_move = False
+        self.turns_before_spec_attack = 0
+        self.character.Stats["HP"] = self.character.Stats["Max_HP"]
 
 
+class Fight:
+    def __init__(self, command1: (list, tuple), command2: (list, tuple)):
+        self.commands = []
+        self.commands.append([FightMember(char) for char in command1])
+        self.commands.append([FightMember(char) for char in command2])
+        self.end = False
+        self.turn = 0
+
+    def make_move(self, n_actor, n_object, move_type):
+        actor, object = None, None
+        for char in self.commands[self.turn]:
+            if char.character.name == n_actor:
+                actor = char
+                break
+        for char in self.commands[abs(self.turn - 1)]:
+            if char.character.name == n_object:
+                object = char
+                break
+        if actor is None or object is None:
+            return "Выбран не существующий персонаж"
+
+        if move_type == "specattack":
+            if not actor.turns_before_spec_attack:
+                damage = actor.character.SpecAttack(object.character)
+                actor.turns_before_spec_attack = actor.character.Spec_Attack_Recovery
+
+        elif move_type == "attack":
+            damage = actor.character.Attack(object.character)
+        if object.character.Stats["HP"] <= 0:
+            object.defeated = True
+        actor.made_move = True
+        if self.check_win():
+            return "Конец боя"
+        return self.check_end_turn()
+
+    def check_end_turn(self):
+        if all([char.made_move for char in self.commands[self.turn]]):
+            self.turn = abs(self.turn - 1)
+            for char in self.commands[self.turn]:
+                char.made_move = False
+                char.turns_before_spec_attack -= 1
+            return "Конец хода"
+        return None
+
+    def check_win(self):
+        self.end = all([char.defeated for char in self.commands[abs(self.turn - 1)]])
