962cbff45d4d1fdc84cb39f546706eb38cb48223
Alexandr <Formak21@ya.ru>
2022-01-10 22:59:49 +0300

    Gui Update
    Gui struct update

--- ThorpyTest.py
+++ ThorpyTest.py
@@ -3,31 +3,44 @@ import thorpy
 application = thorpy.Application((1440, 1024), "ThorPy Overview")
 
 Levels = [thorpy.make_button(f"Уровень {i + 1}") for i in range(10)]
-for l in Levels:
-    l.set_size((350, 120))
-LevelBox = thorpy.Box(elements=Levels)
-LevelBox.add_lift()
-LevelBox.center()
-LevelBox.set_size((1000, 1024))
+Levels_Box = thorpy.Box(elements=Levels)
+for level in Levels:
+    level.set_size((350, 120))
+Levels_Box.set_size((1000, 1024))
 
+Levels_Title = thorpy.make_text("Уровни", 48)
+Levels_Box.add_element(Levels_Title)
+Levels_Title.set_topleft((410, 61))
 
-LevelBox.set_title(thorpy.Title("Уровни"))
 for i in range(len(Levels)):
     if i % 2 == 0:
-        Levels[i].set_topleft((600, -114 + 160 * (i // 2)))
+        Levels[i].set_topleft((100, 160 + 160 * (i // 2)))
     else:
-        Levels[i].set_topleft((1000, -114 + 160 * (i // 2)))
-Characters = [thorpy.make_button(f"Персонаж {i + 1}") for i in range(10)]
+        Levels[i].set_topleft((550, 160 + 160 * (i // 2)))
 
-CharacterBox = thorpy.Box(elements=Characters)
-CharacterBox.set_size((440, 1024))
+Levels_Box.set_topleft((0, 0))
 
-LevelBox.set_topleft((0, 0))
-CharacterBox.set_topleft((1000, 0))
 
-background = thorpy.Background(elements=[LevelBox, CharacterBox])
-# thorpy.store(background)
+Characters = [thorpy.make_button(f"Персонаж {i + 1}") for i in range(4)]
+Characters_Box = thorpy.Box(elements=Characters)
+for character in Characters:
+    character.set_size((120, 120))
+Characters_Box.set_size((440, 1024))
 
+Characters_Title = thorpy.make_text("Персонажи", 48)
+Characters_Box.add_element(Characters_Title)
+Characters_Title.set_topleft((80, 51))
+
+for i in range(len(Characters)):
+    if i % 2 == 0:
+        Characters[i].set_topleft((50, 160 + 160 * (i // 2)))
+    else:
+        Characters[i].set_topleft((245, 160 + 160 * (i // 2)))
+
+Characters_Box.set_topleft((1000, 0))
+
+
+background = thorpy.Background(elements=[Levels_Box, Characters_Box])
 menu = thorpy.Menu(background)
 menu.play()
 
